2d9a8d389ad2dac3e9a1dfa9e2a7b1f0a63cf56c
Oleksiy Pokormyakho <116438996+OleksiyPok@users.noreply.github.com>
2023-03-09 18:13:12 +0100

    remove lodash

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import debounce from 'lodash.debounce';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
